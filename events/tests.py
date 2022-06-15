b293efa467e9da95f040bd6e3b22e3802357feec
mreddy1993 <mahesh.reddy.tamu@gmail.com>
2022-06-14 20:23:41 -0500

    Added Unit test for Event Create

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import eventModels
 
-# Create your tests here.
+class eventTests(TestCase):
+    @classmethod
+    
+    def test_Event_Content(self):
+        def test_createEvent(self):
+            response = self.client.post(
+                reverse("create"),
+                {
+                    "title": "test1",
+                    "date": "1/2/2022",
+                    "eventDescription": "test description",
+                    "location": "Narnia"
+                }   
+            )
+            self.assertEquals(response.status_code, 302)
+            self.assertEquals(eventModels.objects.last().title(),"test1")
+      
+      
+      
+      
+      
+      
+        # Create your tests here.
