0fa158cdfd78845ddb42f5170c0321b9ca6845a2
mreddy1993 <mahesh.reddy.tamu@gmail.com>
2022-06-18 10:57:24 -0500

    tests

--- events/tests.py
+++ events/tests.py
@@ -6,40 +6,38 @@ from .models import eventModels
 class eventTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = get_user_model.objects.create_user(username="testuser", email = "test@email.com", password = "password" )
-        cls.eventModels = eventModels.obejects.create(title= "New", date = "1/1/2020", eventDescription = "test description", location = "somewhere")
-    
-    
-    def test_Event_Content(self):
-        def test_createEvent(self):
-            response = self.client.post(
-                reverse("create"),
-                {
-                    "title": "test1",
-                    "date": "1/2/2022",
-                    "eventDescription": "test description",
-                    "location": "Narnia"
-                }   
-            )
-            self.assertEquals(response.status_code, 302)
-            self.assertEquals(eventModels.objects.last().title(),"test1")
-      
-        def test_updateEvent(self):
-            response = self.client.post(
-            reverse("update", args = 1),
+        cls.user = get_user_model().objects.create_user(username="testuser", email = "test@email.com", password = "password" )
+        cls.eventModels = eventModels.objects.create(title= "New", date = "2022-01-01", eventDescription = "test description", location = "somewhere") 
+
+    def test_createEvent(self):
+        response = self.client.post(
+            reverse("create"),
             {
-                "title": "Updated Title",
-                "location": "Middle Earth"
-            }
+                "title": "test1",
+                "date": "2022-01-01",
+                "eventDescription": "test description",
+                "location": "Narnia"
+            }   
+        )
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(eventModels.objects.last().title,"test1")
+    
+    def test_updateEvent(self):
+        response = self.client.post(
+        reverse("update", args = "1"),
+        {
+            "title": "Updated Title",
+            "location": "Middle Earth"
+        }
 
-            )
+        )
 
-            self.assertEquals(response.status_code, 302)
-            self.assertEquals(eventModels.objects.last().title(), "Updated Title")
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(eventModels.objects.last().title, "Updated Title")
 
-        def test_deleteEvent(self):
-            response = self.client.post("delete", args = 1)
-            self.assertEquals(response.status_code, 302) 
+    def test_deleteEvent(self):
+        response = self.client.post("delete", args = "1")
+        self.assertEquals(response.status_code, 302) 
 
       
       
